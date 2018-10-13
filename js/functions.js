4dfc15dbf7df26e6fa8d978c73eb10747641a4c9
FardinDev <phpfardin@gmail.com>
2018-10-13 19:06:17 +0600

    first commit

---
+++ js/functions.js
@@ -0,0 +1,117 @@
+var pContainerHeight = $('.background').height();
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if (wScroll <= pContainerHeight) {
+
+    $('.layer-4').css({
+      'transform' : 'translate(0px, '+ wScroll /7 +'%)',
+      'filter' : 'blur('+ wScroll/180+'px)'
+
+    });
+
+    $('.layer-3').css({
+      'transform' : 'translate(0px, '+ wScroll /15 +'%)'
+
+
+    });
+
+    $('.layer-2').css({
+      'transform' : 'translate(0px, '+ wScroll /25+'%)'
+
+
+    });
+
+    $('.layer-1').css({
+      'transform' : 'translate(0px, '+ wScroll /45 +'%)'
+    });
+
+    $('.bird-layer').css({
+      'transform' : 'perspective(500px) translateZ('+ wScroll/2+'px) translateX('+ wScroll/2.5+'px)',
+      'filter' : 'blur('+ wScroll/500+'px)'
+      // 'transform' : 'translate('+ wScroll /5 +'%, -'+ wScroll /100 +'%)'
+    });
+
+  }
+
+  if(wScroll >= $('.content').offset().top){
+
+    $('.navbar').addClass('nav-show');
+
+  }
+  else{
+
+    $('.navbar').removeClass( 'nav-show' );
+  }
+
+  if(wScroll >= $('.body-title').offset().top){
+
+    $('.title').addClass('title-show');
+
+  }
+  else{
+
+    $('.title').removeClass( 'title-show' );
+  }
+
+  if(wScroll >= $('.container').offset().top-40){
+
+    $('.title').addClass('title-left');
+    // $('.img').addClass('img-animate');
+    $('.img').css({
+      'transform' : 'translateX(0px)'
+    });
+
+
+  }else{
+
+      $('.title').removeClass( 'title-left' );
+      // $('.img').removeClass('img-animate');
+      $('.img').css({
+        'transform' : 'translateX(-200px)'
+      });
+    }
+
+
+  
+
+
+  //
+  // // Landing Elements
+  // if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)) {
+  //
+  //   $('.clothes-pics figure').each(function(i){
+  //
+  //     setTimeout(function(){
+  //       $('.clothes-pics figure').eq(i).addClass('is-showing');
+  //     }, (700 * (Math.exp(i * 0.14))) - 700);
+  //   });
+  //
+  // }
+  //
+  //
+  // // Promoscope
+  // if(wScroll > $('.large-window').offset().top - $(window).height()){
+  //
+  //   $('.large-window').css({'background-position':'center '+ (wScroll - $('.large-window').offset().top) +'px'});
+  //
+  //   var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+  //
+  //   $('.window-tint').css({'opacity': opacity});
+  //
+  // }
+  //
+  //
+  // // Floating Elements
+  //
+  // if(wScroll > $('.blog-posts').offset().top - $(window).height()){
+  //
+  //   var offset = (Math.min(0, wScroll - $('.blog-posts').offset().top +$(window).height() - 350)).toFixed();
+  //
+  //   $('.post-1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.2) +'px)'});
+  //
+  //   $('.post-3').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.2) +'px)'});
+  //
+  // }
+});
